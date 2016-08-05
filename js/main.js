3535c2211df98f8389cd900ad244a9732776f157
David Scales <davidsscales@gmail.com>
2016-08-05 12:16:55 -0700

    Add: custom event test

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 (function() {
 
+  var button = document.getElementById('buy');
+  button.addEventListener('click', function(event) {
+    console.log('button clicked!');
+    console.log(event);
+
+    ga('send', {
+      hitType: 'event',
+      eventCategory: 'purchase stuff',
+      eventAction: 'buy now',
+      eventLabel: 'store front'
+    });
+
+  });
+
 })();
