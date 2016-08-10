359ab12141fbb06425e3cc64a51aadcc1780e563
David Scales <davidsscales@gmail.com>
2016-08-09 17:34:21 -0700

    Add: notifications

--- js/main.js
+++ js/main.js
@@ -1,5 +1,67 @@
 (function() {
 
+  // Check for service worker support
+  if (!('serviceWorker' in navigator)) {
+    console.log('This browser does not support service worker');
+    // service worker not supported GA
+    return;
+  }
+
+  // Register service worker
+  navigator.serviceWorker.register('service-worker.js')
+  .then(function(registration) {
+    console.log('Service worker registered', registration);
+  })
+  .catch(function(error) {
+    console.log('Service Worker registration failed:', error);
+  });
+
+  // Check for notification support
+  if (!('Notification' in window)) {
+    console.log('This browser does not support notifications!');
+    // notifications not supported GA
+    return;
+  }
+
+  // Request notification permission
+  Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+  });
+  // Notification permission denied GA?
+
+  // Add notification generation to UI button
+  document.getElementById('display').addEventListener('click', function(event) {
+    displayNotification();
+  });
+
+  // Display a standard notification
+  // THIS IS A MANUAL NOTIFICATION, BUT WE also have push one too
+  function displayNotification() {
+    if (Notification.permission == 'granted') {
+      navigator.serviceWorker.getRegistration()
+      .then(function(registration) {
+        var options = {
+          body: 'You have a new notification!',
+          icon: 'images/notification-flat.png',
+          vibrate: [100, 50, 100],
+          data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+          },
+          actions: [
+            {action: 'open', title: 'Open the site!',
+              icon: 'images/checkmark.png'},
+            {action: 'close', title: 'Go away!',
+              icon: 'images/xmark.png'},
+          ]
+        };
+        registration.showNotification('Hello world!', options);
+        // Show notification manual GA
+      });
+    }
+  }
+
+  // Add custom event to UI button
   var button = document.getElementById('buy');
   button.addEventListener('click', function(event) {
     console.log('button clicked!');
---
+++ service-worker.js
@@ -0,0 +1,51 @@
+// THIS IS NOT WORKING
+self.addEventListener('notificationclose', function(event) {
+  var notification = event.notification;
+  var primaryKey = notification.data.primaryKey;
+  console.log('Closed notification: ' + primaryKey);
+  // Notification closed GA
+});
+
+self.addEventListener('notificationclick', function(event) {
+  var notification = event.notification;
+  var primaryKey = notification.data.primaryKey;
+  var action = event.action;
+  if (action === 'close') {
+    notification.close();
+    // Notification close GA - already done in close event
+  } else if (action === 'open') {
+    clients.openWindow('samples/page' + primaryKey + '.html');
+    notification.close();
+    // Notification opened GA
+  } else {
+    clients.openWindow('samples/page' + primaryKey + '.html');
+    notification.close();
+    // Notification clicked GA
+  }
+});
+
+self.addEventListener('push', function(event) {
+  // Push event recieved GA
+  var options = {
+    body: 'This notification was generated from a push!',
+    icon: 'images/notification-flat.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '2'
+    },
+    actions: [
+      {action: 'open', title: 'Open the site!',
+        icon: 'images/checkmark.png'},
+      {action: 'close', title: 'Go away!',
+        icon: 'images/xmark.png'},
+    ]
+  };
+  event.waitUntil(
+    self.registration.showNotification('Hello world!', options)
+  )
+  .then(function() {
+    // Show notification push GA
+  });
+
+});
