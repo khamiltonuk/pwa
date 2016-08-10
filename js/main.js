77bcd8af8a9446eb193c9f82e680ee27cfa78ea1
David Scales <davidsscales@gmail.com>
2016-08-10 10:19:28 -0700

    Refactor: overhaul code to support student TODOs

--- js/main.js
+++ js/main.js
@@ -1,81 +1,52 @@
 (function() {
+  'use strict';
 
-  // Check for service worker support
   if (!('serviceWorker' in navigator)) {
     console.log('This browser does not support service worker');
-    // service worker not supported GA
     return;
   }
 
-  // Check for notification support
   if (!('Notification' in window)) {
     console.log('This browser does not support notifications!');
-    // notifications not supported GA
     return;
   }
 
-  // Register service worker
+  var registration; // Store service worker registration
   navigator.serviceWorker.register('service-worker.js')
-  .then(function(registration) {
-    console.log('Service worker registered', registration);
-
-    // registration.pushManager.subscribe({userVisibleOnly:true}).then(function(sub) {console.log(sub);});
+  .then(function(reg) {
+    console.log('Service worker registered', reg);
+    registration = reg;
   })
   .catch(function(error) {
     console.log('Service Worker registration failed:', error);
   });
 
-  // Request notification permission
+  var purchaseButton = document.getElementById('purchase');
+  purchaseButton.addEventListener('click', function() {
+
+    // TODO Step x: Add custom Google Analytics event
+    ga('send', {
+      hitType: 'event',
+      eventCategory: 'products',
+      eventAction: 'purchase',
+      eventLabel: 'Summer products launch'
+    });
+    // no need console.log
+
+  });
+
+  // TODO Step x: Request notification permission
+  // Browser may prompt user with permission request
   Notification.requestPermission(function(status) {
     console.log('Notification permission status:', status);
   });
-  // Notification permission denied GA?
 
-  // Check for push notification (SHOULD THIS JUST BE IN SW REGISTRATION?)
-  navigator.serviceWorker.ready.then(function(registration) {
-    registration.pushManager.getSubscription()
-    .then(function(subscription) {
-
-      if (!subscription) {
-        if (window.confirm(
-          'Would you like to subscribe to push notifications?')) {
-          // subscribe accept GA
-          console.log('sub yes');
-          registration.pushManager.subscribe({
-            userVisibleOnly: true
-          })
-          .then(function(subscription) {
-            // supscribe successful GA
-            console.log('sub complete');
-          })
-          .catch(function(error) {
-            if (Notification.permission === 'denied') {
-              console.warn('Permission for notifications was denied');
-              // subscribe failed GA, notifications
-            } else {
-              console.log('Unable to subscribe to Push!', error);
-              // subscribe failed GA, error
-            }
-          });
-        } else {
-          // Push subscription rejected GA
-          console.log('sub rejected');
-        }
-      } else {
-        console.log('User already subscribed', subscription);
-        // subscription.unsubscribe();
-      }
-
-    });
-  });
-
-  // Add notification generation to UI button
-  document.getElementById('display').addEventListener('click', function(event) {
+  // TODO Step x: Add manual notification generation
+  var notifyButton = document.getElementById('notify');
+  notifyButton.addEventListener('click', function() {
     displayNotification();
   });
 
-  // Display a standard notification
-  // THIS IS A MANUAL NOTIFICATION, BUT WE also have push one too
   function displayNotification() {
     if (Notification.permission == 'granted') {
       navigator.serviceWorker.getRegistration()
@@ -96,24 +67,55 @@
           ]
         };
         registration.showNotification('Hello world!', options);
-        // Show notification manual GA
+        console.log('Manual notification displayed');
       });
+    } else {
+      console.log('Notification failed, user has notifications blocked');
     }
   }
 
-  // Add custom event to UI button
-  var button = document.getElementById('buy');
-  button.addEventListener('click', function(event) {
-    console.log('button clicked!');
-    console.log(event);
+  // TODO Step x: Add push notification
 
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'products',
-      eventAction: 'view details',
-      eventLabel: 'Summer products launch'
-    });
+  var subscription;
+  var isSubscribed = false;
+  var subscribeButton = document.getElementById('subscribe');
 
+  subscribeButton.addEventListener('click', function() {
+    if (isSubscribed) {
+      unsubscribe();
+    } else {
+      subscribe();
+    }
   });
+
+  function subscribe() {
+    registration.pushManager.subscribe({userVisibleOnly: true})
+    .then(function(pushSubscription) {
+      subscription = pushSubscription;
+      console.log('Subscribed!', subscription);
+      subscribeButton.textContent = 'Unsubscribe';
+      isSubscribed = true;
+    })
+    .catch(function(error) {
+      if (Notification.permission === 'denied') {
+        console.log('Subscribe failed, user has notifications blocked');
+      } else {
+        console.log('Error subscribing', error);
+      }
+    });
+  }
+
+  function unsubscribe() {
+    subscription.unsubscribe()
+    .then(function() {
+      console.log('Unsubscribed!');
+      subscribeButton.textContent = 'Subscribe';
+      isSubscribed = false;
+    })
+    .catch(function(error) {
+      console.log('Error unsubscribing', error);
+      subscribeButton.textContent = 'Subscribe';
+    });
+  }
 
 })();
--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,59 @@
-// THIS IS NOT WORKING
-self.addEventListener('notificationclose', function(event) {
-  var notification = event.notification;
-  var primaryKey = notification.data.primaryKey;
-  console.log('Closed notification: ' + primaryKey);
-  // Notification closed GA
-});
+(function() {
+  'use strict';
 
-self.addEventListener('notificationclick', function(event) {
-  var notification = event.notification;
-  var primaryKey = notification.data.primaryKey;
-  var action = event.action;
-  if (action === 'close') {
-    notification.close();
-    // Notification close GA - already done in close event
-  } else if (action === 'open') {
-    clients.openWindow('samples/page' + primaryKey + '.html');
-    notification.close();
-    // Notification opened GA
-  } else {
-    clients.openWindow('samples/page' + primaryKey + '.html');
-    notification.close();
-    // Notification clicked GA
-  }
-});
+  // THIS IS NOT WORKING
+  // TODO Step x: Add notification close listener
+  self.addEventListener('notificationclose', function(event) {
+    var notification = event.notification;
+    var primaryKey = notification.data.primaryKey;
+    console.log('Closed notification:', primaryKey);
+  });
 
-self.addEventListener('push', function(event) {
-  // Push event recieved GA
-  var options = {
-    body: 'This notification was generated from a push!',
-    icon: 'images/notification-flat.png',
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: '2'
-    },
-    actions: [
-      {action: 'open', title: 'Open the site!',
-        icon: 'images/checkmark.png'},
-      {action: 'close', title: 'Go away!',
-        icon: 'images/xmark.png'},
-    ]
-  };
-  event.waitUntil(
-    self.registration.showNotification('Hello world!', options)
-  );
-  // Show notification push GA
+  // TODO Step x: Add notification click listener
+  self.addEventListener('notificationclick', function(event) {
+    var notification = event.notification;
+    var primaryKey = notification.data.primaryKey;
+    var action = event.action;
+    if (action === 'close') {
+      notification.close();
+      // Notification close GA - already done in close event
+    } else if (action === 'open') {
+      clients.openWindow('pages/page' + primaryKey + '.html');
+      notification.close();
+      console.log('Notification accepted');
+    } else {
+      clients.openWindow('pages/page' + primaryKey + '.html');
+      notification.close();
+      console.log('Notification clicked');
+    }
+  });
 
-});
+  // TODO Step x: Add push listener
+  self.addEventListener('push', function(event) {
+    console.log('Push recieved');
+    if (Notification.permission === 'denied') {
+      console.log('Push notification failed, user has notifications blocked');
+      return;
+    }
+    var options = {
+      body: 'This notification was generated from a push!',
+      icon: 'images/notification-flat.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: '2'
+      },
+      actions: [
+        {action: 'open', title: 'Open the site!',
+          icon: 'images/checkmark.png'},
+        {action: 'close', title: 'Go away!',
+          icon: 'images/xmark.png'},
+      ]
+    };
+    event.waitUntil(
+      self.registration.showNotification('Hello world!', options)
+    );
+    console.log('Push notification displayed');
+  });
+
+})();
