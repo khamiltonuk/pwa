f8a347a50b0b342f2f61b8b4222de93349a738c8
David Scales <davidsscales@gmail.com>
2016-08-11 16:00:55 -0700

    working step 8

--- js/main.js
+++ js/main.js
@@ -3,16 +3,32 @@
 
   if (!('Notification' in window)) {
     console.log('This browser does not support notifications!');
+    // TODO Optional step 8g: Send notification unsupported event
     ga('send', 'event', 'notifications', 'unsupported');
     return;
   }
 
   if (!('serviceWorker' in navigator)) {
     console.log('This browser does not support service worker');
+    // TODO Optional step 8h: Send service worker unsupported event
     ga('send', 'event', 'service-worker', 'unsupported');
     return;
   }
 
+  var purchaseButton = document.getElementById('purchase');
+  purchaseButton.addEventListener('click', function() {
+
+    // TODO Step 7: Send custom Google Analytics event
+    ga('send', {
+      hitType: 'event',
+      eventCategory: 'products',
+      eventAction: 'purchase',
+      eventLabel: 'Summer products launch'
+    });
+
+  });
+
+  // TODO Step 8a: Register the service worker
   var registration; // Store service worker registration
   navigator.serviceWorker.register('service-worker.js')
   .then(function(reg) {
@@ -23,126 +39,119 @@
     console.log('Service Worker registration failed:', error);
   });
 
-  var purchaseButton = document.getElementById('purchase');
-  purchaseButton.addEventListener('click', function() {
-
-    // TODO Step x: Add custom Google Analytics event
-    // ga('send', {
-    //   hitType: 'event',
-    //   eventCategory: 'products',
-    //   eventAction: 'purchase',
-    //   eventLabel: 'Summer products launch'
-    // });
-
+  // TODO Step 8b: Add notifications
+  // Browser may prompt user with permission request
+  Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
   });
 
-  // TODO Step x: Request notification permission
-  // Browser may prompt user with permission request
-  // Notification.requestPermission(function(status) {
-  //   console.log('Notification permission status:', status);
-  // });
+  // TODO Step 8c: Add manual notification trigger to button
+  var notifyButton = document.getElementById('notify');
+  notifyButton.addEventListener('click', function() {
+    displayNotification();
+  });
 
-  // TODO Step x: Add manual notification generation
-  // var notifyButton = document.getElementById('notify');
-  // notifyButton.addEventListener('click', function() {
-  //   displayNotification();
-  // });
-  //
-  // function displayNotification() {
-  //   if (Notification.permission == 'granted') {
-  //     navigator.serviceWorker.getRegistration()
-  //     .then(function(registration) {
-  //       var options = {
-  //         body: 'You have a new notification!',
-  //         icon: 'images/notification-flat.png',
-  //         vibrate: [100, 50, 100],
-  //         data: {
-  //           dateOfArrival: Date.now(),
-  //           primaryKey: 1
-  //         },
-  //         actions: [
-  //           {action: 'open', title: 'Open the site!',
-  //             icon: 'images/checkmark.png'},
-  //           {action: 'close', title: 'Go away!',
-  //             icon: 'images/xmark.png'},
-  //         ]
-  //       };
-  //       registration.showNotification('Hello world!', options);
-  //       console.log('Standard notification displayed');
-  //       ga('send', 'event', 'notification', 'displayed-standard');
-  //     });
-  //   } else {
-  //     console.log('Notification failed, user has notifications blocked');
-  //     ga('send', 'event', 'notification', 'blocked');
-  //   }
-  // }
+  // TODO Step 8d: Display manual notifications
+  function displayNotification() {
+    if (Notification.permission == 'granted') {
+      navigator.serviceWorker.getRegistration()
+      .then(function(registration) {
+        var options = {
+          body: 'You have a new notification!',
+          icon: 'images/notification-flat.png',
+          vibrate: [100, 50, 100],
+          data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+          },
+          actions: [
+            {action: 'open', title: 'Open the site!',
+              icon: 'images/checkmark.png'},
+            {action: 'close', title: 'Go away!',
+              icon: 'images/xmark.png'},
+          ]
+        };
+        registration.showNotification('Hello world!', options);
+        // TODO Step 8e: send notification display event
+        ga('send', 'event', 'notification', 'displayed-standard');
+      });
+    } else {
+      console.warn('Notifications are blocked');
+      // TODO Step 8f: send notification blocked event
+      ga('send', 'event', 'notification', 'blocked');
+    }
+  }
 
-  // TODO Step x: Open communication with service worker
-  // function openCommunication() {
-  //   var msgChannel = new MessageChannel();
-  //   msgChannel.port1.onmessage = function(msgEvent) {
-  //     sendAnalytics(msgEvent.data);
-  //   };
-  //   navigator.serviceWorker.controller.postMessage(
-  //     'Establishing contact with service worker', [msgChannel.port2]
-  //   );
-  // }
-  //
-  // openCommunication();
-  //
-  // function sendAnalytics(message) {
-  //   var eventCategory = message.eventCategory;
-  //   var eventAction = message.eventAction;
-  //   ga('send', 'event', eventCategory, eventAction);
-  // }
+  // TODO Step 8i: Open communication with service worker
+  function openCommunication() {
+    var msgChannel = new MessageChannel();
+    msgChannel.port1.onmessage = function(msgEvent) {
+      sendAnalytics(msgEvent.data);
+    };
+    navigator.serviceWorker.controller.postMessage(
+      'Establishing contact with service worker', [msgChannel.port2]
+    );
+  }
+  openCommunication();
 
-  // TODO Step x: Add push notification
+  function sendAnalytics(message) {
+    var eventCategory = message.eventCategory;
+    var eventAction = message.eventAction;
+    ga('send', 'event', eventCategory, eventAction);
+  }
 
-  // var subscription;
-  // var isSubscribed = false;
-  // var subscribeButton = document.getElementById('subscribe');
-  //
-  // subscribeButton.addEventListener('click', function() {
-  //   if (isSubscribed) {
-  //     unsubscribe();
-  //   } else {
-  //     subscribe();
-  //   }
-  // });
-  //
-  // function subscribe() {
-  //   registration.pushManager.subscribe({userVisibleOnly: true})
-  //   .then(function(pushSubscription) {
-  //     subscription = pushSubscription;
-  //     console.log('Subscribed!', subscription);
-  //     ga('send', 'event', 'push', 'subscribe');
-  //     subscribeButton.textContent = 'Unsubscribe';
-  //     isSubscribed = true;
-  //   })
-  //   .catch(function(error) {
-  //     if (Notification.permission === 'denied') {
-  //       console.warn('Subscribe failed, notifications are blocked');
-  //       ga('send', 'event', 'push', 'subscribe-blocked');
-  //     } else {
-  //       console.log('Error subscribing', error);
-  //       ga('send', 'event', 'push', 'subscribe-error');
-  //     }
-  //   });
-  // }
-  //
-  // function unsubscribe() {
-  //   subscription.unsubscribe()
-  //   .then(function() {
-  //     console.log('Unsubscribed!');
-  //     ga('send', 'event', 'push', 'unsubscribe');
-  //     subscribeButton.textContent = 'Subscribe';
-  //     isSubscribed = false;
-  //   })
-  //   .catch(function(error) {
-  //     console.log('Error unsubscribing', error);
-  //     ga('send', 'event', 'push', 'unsubscribe-error');
-  //     subscribeButton.textContent = 'Subscribe';
-  //   });
-  // }
+  // TODO Step 9b: Add push notification
+
+  var subscription;
+  var isSubscribed = false;
+  var subscribeButton = document.getElementById('subscribe');
+
+  subscribeButton.addEventListener('click', function() {
+    if (isSubscribed) {
+      unsubscribe();
+    } else {
+      subscribe();
+    }
+  });
+
+  function subscribe() {
+    registration.pushManager.subscribe({userVisibleOnly: true})
+    .then(function(pushSubscription) {
+      subscription = pushSubscription;
+      console.log('Subscribed!', subscription);
+      // TODO Step 9d: send push subscribed event
+      // ga('send', 'event', 'push', 'subscribe');
+      subscribeButton.textContent = 'Unsubscribe';
+      isSubscribed = true;
+    })
+    .catch(function(error) {
+      if (Notification.permission === 'denied') {
+        console.warn('Subscribe failed, notifications are blocked');
+        // TODO Step 9e: send push blocked event
+        // ga('send', 'event', 'push', 'subscribe-blocked');
+      } else {
+        console.log('Error subscribing', error);
+        // TODO Step 9f: send push subscribe-error event
+        // ga('send', 'event', 'push', 'subscribe-error');
+      }
+    });
+  }
+
+  function unsubscribe() {
+    subscription.unsubscribe()
+    .then(function() {
+      console.log('Unsubscribed!');
+      // TODO Step 9g: send push unsubscibe event
+      // ga('send', 'event', 'push', 'unsubscribe');
+      subscribeButton.textContent = 'Subscribe';
+      isSubscribed = false;
+    })
+    .catch(function(error) {
+      console.log('Error unsubscribing', error);
+      // TODO Step 9h: send push unsubscribe-error event
+      // ga('send', 'event', 'push', 'unsubscribe-error');
+      subscribeButton.textContent = 'Subscribe';
+    });
+  }
 
 })();
--- service-worker.js
+++ service-worker.js
@@ -2,91 +2,97 @@
   'use strict';
 
   // THIS IS NOT WORKING
-  // TODO Step x: Add notification close listener
-  // self.addEventListener('notificationclose', function(event) {
-  //   var notification = event.notification;
-  //   var primaryKey = notification.data.primaryKey;
-  //   console.log('Closed notification:', primaryKey);
-  //   mainClientPort.postMessage({
-  //     eventCategory: 'notification',
-  //     eventAction: 'closed'
-  //   });
-  // });
+  // TODO Step 8g: Add notification close listener
+  self.addEventListener('notificationclose', function(event) {
+    var notification = event.notification;
+    var primaryKey = notification.data.primaryKey;
+    console.log('Closed notification:', primaryKey);
+    // TODO Step 8k: send notification close event
+    mainClientPort.postMessage({
+      eventCategory: 'notification',
+      eventAction: 'closed'
+    });
+  });
 
-  // TODO Step x: Add notification click listener
-  // self.addEventListener('notificationclick', function(event) {
-  //   var notification = event.notification;
-  //   var primaryKey = notification.data.primaryKey;
-  //   var action = event.action;
-  //   if (action === 'close') {
-  //     notification.close();
-  //     // Notification close GA - already done in close event
-  //   } else if (action === 'open') {
-  //     clients.openWindow('pages/page' + primaryKey + '.html');
-  //     notification.close();
-  //     console.log('Notification opened'); // action?
-  //     mainClientPort.postMessage({
-  //       eventCategory: 'notification',
-  //       eventAction: 'opened'
-  //     });
-  //   } else {
-  //     clients.openWindow('pages/page' + primaryKey + '.html');
-  //     notification.close();
-  //     console.log('Notification clicked');
-  //     mainClientPort.postMessage({
-  //       eventCategory: 'notification',
-  //       eventAction: 'clicked'
-  //     });
-  //   }
-  // });
+  // TODO Step 8h: Add notification click listener
+  self.addEventListener('notificationclick', function(event) {
+    var notification = event.notification;
+    var primaryKey = notification.data.primaryKey;
+    var action = event.action;
+    if (action === 'close') {
+      notification.close();
+      // TODO // notificationclose listener sends notification close event
+    } else if (action === 'open') {
+      clients.openWindow('pages/page' + primaryKey + '.html');
+      notification.close();
+      console.log('Notification opened');
+      // TODO Step 8l: Send notification opened event
+      mainClientPort.postMessage({
+        eventCategory: 'notification',
+        eventAction: 'opened'
+      });
+    } else {
+      clients.openWindow('pages/page' + primaryKey + '.html');
+      notification.close();
+      console.log('Notification clicked');
+      // TODO Step 8m: Send notification clicked event
+      mainClientPort.postMessage({
+        eventCategory: 'notification',
+        eventAction: 'clicked'
+      });
+    }
+  });
 
-  // TODO Step x: Add message listener & establish communication
-  // var mainClientPort;
-  // self.addEventListener('message', function(event) {
-  //   console.log('Service worker received message: ', event.data);
-  //   mainClientPort = event.ports[0];
-  //   console.log('Communication ports established');
-  // });
+  // TODO Step 8j: Add message listener & establish communication
+  var mainClientPort;
+  self.addEventListener('message', function(event) {
+    console.log('Service worker received message: ', event.data);
+    mainClientPort = event.ports[0];
+    console.log('Communication ports established');
+  });
 
-  // TODO Step x: Add push listener
-  // self.addEventListener('push', function(event) {
-  //   console.log('Push recieved');
-  //   mainClientPort.postMessage({
-  //     eventCategory: 'push',
-  //     eventAction: 'recieved'
-  //   });
-  //   if (Notification.permission === 'denied') {
-  //     console.warn('Push notification failed, notifications are blocked');
-  //     mainClientPort.postMessage({
-  //       eventCategory: 'push',
-  //       eventAction: 'blocked'
-  //     });
-  //     return;
-  //   }
-  //   var options = {
-  //     body: 'This notification was generated from a push!',
-  //     icon: 'images/notification-flat.png',
-  //     vibrate: [100, 50, 100],
-  //     data: {
-  //       dateOfArrival: Date.now(),
-  //       primaryKey: '2'
-  //     },
-  //     actions: [
-  //       {action: 'open', title: 'Open the site!',
-  //         icon: 'images/checkmark.png'},
-  //       {action: 'close', title: 'Go away!',
-  //         icon: 'images/xmark.png'},
-  //     ]
-  //   };
-  //   event.waitUntil(
-  //     Promise.all([
-  //       self.registration.showNotification('Hello world!', options),
-  //       mainClientPort.postMessage({
-  //         eventCategory: 'notification',
-  //         eventAction: 'displayed-push'
-  //       })
-  //     ])
-  //   );
-  // });
+  // TODO Step 9c: Add push listener
+  self.addEventListener('push', function(event) {
+    console.log('Push recieved');
+    // TODO Step 9i: Send push received event
+    // mainClientPort.postMessage({
+    //   eventCategory: 'push',
+    //   eventAction: 'recieved'
+    // });
+    if (Notification.permission === 'denied') {
+      console.warn('Push notification failed, notifications are blocked');
+      // TODO Step 9j: Send push blocked event
+      // mainClientPort.postMessage({
+      //   eventCategory: 'push',
+      //   eventAction: 'blocked'
+      // });
+      return;
+    }
+    var options = {
+      body: 'This notification was generated from a push!',
+      icon: 'images/notification-flat.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: '2'
+      },
+      actions: [
+        {action: 'open', title: 'Open the site!',
+          icon: 'images/checkmark.png'},
+        {action: 'close', title: 'Go away!',
+          icon: 'images/xmark.png'},
+      ]
+    };
+    event.waitUntil(
+      Promise.all([
+        self.registration.showNotification('Hello world!', options),
+        // TODO Step 9k: Send notification displayed-push event
+        // mainClientPort.postMessage({
+        //   eventCategory: 'notification',
+        //   eventAction: 'displayed-push'
+        // })
+      ])
+    );
+  });
 
 })();
