0ec56fc575211e70476950ea706dc96d6686d860
David Scales <davidsscales@gmail.com>
2016-08-11 15:46:39 -0700

    initial commit: starting code

--- js/main.js
+++ js/main.js
@@ -1,21 +1,15 @@
 (function() {
   'use strict';
 
-  if (!('serviceWorker' in navigator)) {
-    console.log('This browser does not support service worker');
-    ga('send', 'event', 'service-worker', 'unsupported');
-    return;
-  }
-
   if (!('Notification' in window)) {
     console.log('This browser does not support notifications!');
     ga('send', 'event', 'notifications', 'unsupported');
     return;
   }
 
-  if (!('Worker' in window)) {
-    console.log('This browser does not support workers');
-    ga('send', 'event', 'workers', 'unsupported');
+  if (!('serviceWorker' in navigator)) {
+    console.log('This browser does not support service worker');
+    ga('send', 'event', 'service-worker', 'unsupported');
     return;
   }
 
@@ -33,122 +27,122 @@
   purchaseButton.addEventListener('click', function() {
 
     // TODO Step x: Add custom Google Analytics event
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'products',
-      eventAction: 'purchase',
-      eventLabel: 'Summer products launch'
-    });
+    // ga('send', {
+    //   hitType: 'event',
+    //   eventCategory: 'products',
+    //   eventAction: 'purchase',
+    //   eventLabel: 'Summer products launch'
+    // });
 
   });
 
   // TODO Step x: Request notification permission
   // Browser may prompt user with permission request
-  Notification.requestPermission(function(status) {
-    console.log('Notification permission status:', status);
-  });
+  // Notification.requestPermission(function(status) {
+  //   console.log('Notification permission status:', status);
+  // });
 
   // TODO Step x: Add manual notification generation
-  var notifyButton = document.getElementById('notify');
-  notifyButton.addEventListener('click', function() {
-    displayNotification();
-  });
-
-  function displayNotification() {
-    if (Notification.permission == 'granted') {
-      navigator.serviceWorker.getRegistration()
-      .then(function(registration) {
-        var options = {
-          body: 'You have a new notification!',
-          icon: 'images/notification-flat.png',
-          vibrate: [100, 50, 100],
-          data: {
-            dateOfArrival: Date.now(),
-            primaryKey: 1
-          },
-          actions: [
-            {action: 'open', title: 'Open the site!',
-              icon: 'images/checkmark.png'},
-            {action: 'close', title: 'Go away!',
-              icon: 'images/xmark.png'},
-          ]
-        };
-        registration.showNotification('Hello world!', options);
-        console.log('Standard notification displayed');
-        ga('send', 'event', 'notification', 'displayed-standard');
-      });
-    } else {
-      console.log('Notification failed, user has notifications blocked');
-      ga('send', 'event', 'notification', 'blocked');
-    }
-  }
+  // var notifyButton = document.getElementById('notify');
+  // notifyButton.addEventListener('click', function() {
+  //   displayNotification();
+  // });
+  //
+  // function displayNotification() {
+  //   if (Notification.permission == 'granted') {
+  //     navigator.serviceWorker.getRegistration()
+  //     .then(function(registration) {
+  //       var options = {
+  //         body: 'You have a new notification!',
+  //         icon: 'images/notification-flat.png',
+  //         vibrate: [100, 50, 100],
+  //         data: {
+  //           dateOfArrival: Date.now(),
+  //           primaryKey: 1
+  //         },
+  //         actions: [
+  //           {action: 'open', title: 'Open the site!',
+  //             icon: 'images/checkmark.png'},
+  //           {action: 'close', title: 'Go away!',
+  //             icon: 'images/xmark.png'},
+  //         ]
+  //       };
+  //       registration.showNotification('Hello world!', options);
+  //       console.log('Standard notification displayed');
+  //       ga('send', 'event', 'notification', 'displayed-standard');
+  //     });
+  //   } else {
+  //     console.log('Notification failed, user has notifications blocked');
+  //     ga('send', 'event', 'notification', 'blocked');
+  //   }
+  // }
 
   // TODO Step x: Open communication with service worker
-  function openCommunication() {
-    var msgChannel = new MessageChannel();
-    msgChannel.port1.onmessage = function(msgEvent) {
-      sendAnalytics(msgEvent.data);
-    };
-    navigator.serviceWorker.controller.postMessage(
-      'Establishing contact with service worker', [msgChannel.port2]
-    );
-  }
-
-  openCommunication();
-
-  function sendAnalytics(message) {
-    var eventCategory = message.eventCategory;
-    var eventAction = message.eventAction;
-    ga('send', 'event', eventCategory, eventAction);
-  }
+  // function openCommunication() {
+  //   var msgChannel = new MessageChannel();
+  //   msgChannel.port1.onmessage = function(msgEvent) {
+  //     sendAnalytics(msgEvent.data);
+  //   };
+  //   navigator.serviceWorker.controller.postMessage(
+  //     'Establishing contact with service worker', [msgChannel.port2]
+  //   );
+  // }
+  //
+  // openCommunication();
+  //
+  // function sendAnalytics(message) {
+  //   var eventCategory = message.eventCategory;
+  //   var eventAction = message.eventAction;
+  //   ga('send', 'event', eventCategory, eventAction);
+  // }
 
   // TODO Step x: Add push notification
 
-  var subscription;
-  var isSubscribed = false;
-  var subscribeButton = document.getElementById('subscribe');
-
-  subscribeButton.addEventListener('click', function() {
-    if (isSubscribed) {
-      unsubscribe();
-    } else {
-      subscribe();
-    }
-  });
-
-  function subscribe() {
-    registration.pushManager.subscribe({userVisibleOnly: true})
-    .then(function(pushSubscription) {
-      subscription = pushSubscription;
-      console.log('Subscribed!', subscription);
-      ga('send', 'event', 'push', 'subscribe');
-      subscribeButton.textContent = 'Unsubscribe';
-      isSubscribed = true;
-    })
-    .catch(function(error) {
-      if (Notification.permission === 'denied') {
-        console.warn('Subscribe failed, notifications are blocked');
-        ga('send', 'event', 'push', 'subscribe-blocked');
-      } else {
-        console.log('Error subscribing', error);
-        ga('send', 'event', 'push', 'subscribe-error');
-      }
-    });
-  }
-
-  function unsubscribe() {
-    subscription.unsubscribe()
-    .then(function() {
-      console.log('Unsubscribed!');
-      ga('send', 'event', 'push', 'unsubscribe');
-      subscribeButton.textContent = 'Subscribe';
-      isSubscribed = false;
-    })
-    .catch(function(error) {
-      console.log('Error unsubscribing', error);
-      ga('send', 'event', 'push', 'unsubscribe-error');
-      subscribeButton.textContent = 'Subscribe';
-    });
-  }
+  // var subscription;
+  // var isSubscribed = false;
+  // var subscribeButton = document.getElementById('subscribe');
+  //
+  // subscribeButton.addEventListener('click', function() {
+  //   if (isSubscribed) {
+  //     unsubscribe();
+  //   } else {
+  //     subscribe();
+  //   }
+  // });
+  //
+  // function subscribe() {
+  //   registration.pushManager.subscribe({userVisibleOnly: true})
+  //   .then(function(pushSubscription) {
+  //     subscription = pushSubscription;
+  //     console.log('Subscribed!', subscription);
+  //     ga('send', 'event', 'push', 'subscribe');
+  //     subscribeButton.textContent = 'Unsubscribe';
+  //     isSubscribed = true;
+  //   })
+  //   .catch(function(error) {
+  //     if (Notification.permission === 'denied') {
+  //       console.warn('Subscribe failed, notifications are blocked');
+  //       ga('send', 'event', 'push', 'subscribe-blocked');
+  //     } else {
+  //       console.log('Error subscribing', error);
+  //       ga('send', 'event', 'push', 'subscribe-error');
+  //     }
+  //   });
+  // }
+  //
+  // function unsubscribe() {
+  //   subscription.unsubscribe()
+  //   .then(function() {
+  //     console.log('Unsubscribed!');
+  //     ga('send', 'event', 'push', 'unsubscribe');
+  //     subscribeButton.textContent = 'Subscribe';
+  //     isSubscribed = false;
+  //   })
+  //   .catch(function(error) {
+  //     console.log('Error unsubscribing', error);
+  //     ga('send', 'event', 'push', 'unsubscribe-error');
+  //     subscribeButton.textContent = 'Subscribe';
+  //   });
+  // }
 
 })();
--- service-worker.js
+++ service-worker.js
@@ -3,91 +3,90 @@
 
   // THIS IS NOT WORKING
   // TODO Step x: Add notification close listener
-  self.addEventListener('notificationclose', function(event) {
-    var notification = event.notification;
-    var primaryKey = notification.data.primaryKey;
-    console.log('Closed notification:', primaryKey);
-    mainClientPort.postMessage({
-      eventCategory: 'notification',
-      eventAction: 'closed'
-    });
-  });
+  // self.addEventListener('notificationclose', function(event) {
+  //   var notification = event.notification;
+  //   var primaryKey = notification.data.primaryKey;
+  //   console.log('Closed notification:', primaryKey);
+  //   mainClientPort.postMessage({
+  //     eventCategory: 'notification',
+  //     eventAction: 'closed'
+  //   });
+  // });
 
   // TODO Step x: Add notification click listener
-  self.addEventListener('notificationclick', function(event) {
-    var notification = event.notification;
-    var primaryKey = notification.data.primaryKey;
-    var action = event.action;
-    if (action === 'close') {
-      notification.close();
-      // Notification close GA - already done in close event
-    } else if (action === 'open') {
-      clients.openWindow('pages/page' + primaryKey + '.html');
-      notification.close();
-      console.log('Notification opened'); // action?
-      mainClientPort.postMessage({
-        eventCategory: 'notification',
-        eventAction: 'opened'
-      });
-    } else {
-      clients.openWindow('pages/page' + primaryKey + '.html');
-      notification.close();
-      console.log('Notification clicked');
-      mainClientPort.postMessage({
-        eventCategory: 'notification',
-        eventAction: 'clicked'
-      });
-    }
-  });
+  // self.addEventListener('notificationclick', function(event) {
+  //   var notification = event.notification;
+  //   var primaryKey = notification.data.primaryKey;
+  //   var action = event.action;
+  //   if (action === 'close') {
+  //     notification.close();
+  //     // Notification close GA - already done in close event
+  //   } else if (action === 'open') {
+  //     clients.openWindow('pages/page' + primaryKey + '.html');
+  //     notification.close();
+  //     console.log('Notification opened'); // action?
+  //     mainClientPort.postMessage({
+  //       eventCategory: 'notification',
+  //       eventAction: 'opened'
+  //     });
+  //   } else {
+  //     clients.openWindow('pages/page' + primaryKey + '.html');
+  //     notification.close();
+  //     console.log('Notification clicked');
+  //     mainClientPort.postMessage({
+  //       eventCategory: 'notification',
+  //       eventAction: 'clicked'
+  //     });
+  //   }
+  // });
 
   // TODO Step x: Add message listener & establish communication
-  var mainClientPort;
-  self.addEventListener('message', function(event) {
-    console.log('Service worker received message: ', event.data);
-    mainClientPort = event.ports[0];
-    console.log('Communication ports established');
-  });
+  // var mainClientPort;
+  // self.addEventListener('message', function(event) {
+  //   console.log('Service worker received message: ', event.data);
+  //   mainClientPort = event.ports[0];
+  //   console.log('Communication ports established');
+  // });
 
   // TODO Step x: Add push listener
-  self.addEventListener('push', function(event) {
-    console.log('Push recieved');
-    mainClientPort.postMessage({
-      eventCategory: 'push',
-      eventAction: 'recieved'
-    });
-
-    if (Notification.permission === 'denied') {
-      console.warn('Push notification failed, notifications are blocked');
-      mainClientPort.postMessage({
-        eventCategory: 'push',
-        eventAction: 'blocked'
-      });
-      return;
-    }
-    var options = {
-      body: 'This notification was generated from a push!',
-      icon: 'images/notification-flat.png',
-      vibrate: [100, 50, 100],
-      data: {
-        dateOfArrival: Date.now(),
-        primaryKey: '2'
-      },
-      actions: [
-        {action: 'open', title: 'Open the site!',
-          icon: 'images/checkmark.png'},
-        {action: 'close', title: 'Go away!',
-          icon: 'images/xmark.png'},
-      ]
-    };
-    event.waitUntil(
-      Promise.all([
-        self.registration.showNotification('Hello world!', options),
-        mainClientPort.postMessage({
-          eventCategory: 'notification',
-          eventAction: 'displayed-push'
-        })
-      ])
-    );
-  });
+  // self.addEventListener('push', function(event) {
+  //   console.log('Push recieved');
+  //   mainClientPort.postMessage({
+  //     eventCategory: 'push',
+  //     eventAction: 'recieved'
+  //   });
+  //   if (Notification.permission === 'denied') {
+  //     console.warn('Push notification failed, notifications are blocked');
+  //     mainClientPort.postMessage({
+  //       eventCategory: 'push',
+  //       eventAction: 'blocked'
+  //     });
+  //     return;
+  //   }
+  //   var options = {
+  //     body: 'This notification was generated from a push!',
+  //     icon: 'images/notification-flat.png',
+  //     vibrate: [100, 50, 100],
+  //     data: {
+  //       dateOfArrival: Date.now(),
+  //       primaryKey: '2'
+  //     },
+  //     actions: [
+  //       {action: 'open', title: 'Open the site!',
+  //         icon: 'images/checkmark.png'},
+  //       {action: 'close', title: 'Go away!',
+  //         icon: 'images/xmark.png'},
+  //     ]
+  //   };
+  //   event.waitUntil(
+  //     Promise.all([
+  //       self.registration.showNotification('Hello world!', options),
+  //       mainClientPort.postMessage({
+  //         eventCategory: 'notification',
+  //         eventAction: 'displayed-push'
+  //       })
+  //     ])
+  //   );
+  // });
 
 })();
