4f44764a6012f8f19523bb45bcf3e96e8c898a8e
David Scales <davidsscales@gmail.com>
2016-08-15 15:26:52 -0700

    section 8 complete

--- js/main.js
+++ js/main.js
@@ -3,32 +3,82 @@
 
   if (!('Notification' in window)) {
     console.log('This browser does not support notifications!');
-    // TODO Optional step 8g: Send notification unsupported event
+    ga('send', 'event', 'notification', 'unsupported');
     return;
   }
 
   if (!('serviceWorker' in navigator)) {
     console.log('This browser does not support service worker');
-    // TODO Optional step 8h: Send service worker unsupported event
+    ga('send', 'event', 'service-worker', 'unsupported');
     return;
   }
 
   var purchaseButton = document.getElementById('purchase');
   purchaseButton.addEventListener('click', function() {
-
-    // TODO Step 7: Send custom Google Analytics event
-
+    ga('send', 'event', 'products', 'purchase', 'Summer product launch');
   });
 
-  // TODO Step 8a: Register the service worker
+  var registration; // Store service worker registration
+  navigator.serviceWorker.register('service-worker.js')
+  .then(function(reg) {
+    console.log('Service worker registered', reg);
+    registration = reg;
+  })
+  .catch(function(error) {
+    console.log('Service Worker registration failed:', error);
+  });
 
-  // TODO Step 8b: Add notifications
+  // Browser may prompt user with permission request
+  Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+  });
 
-  // TODO Step 8c: Add manual notification trigger to button
+  var notifyButton = document.getElementById('notify');
+  notifyButton.addEventListener('click', function() {
+    displayNotification();
+  });
 
-  // TODO Step 8d: Display manual notifications
+  function displayNotification() {
+    if (Notification.permission == 'granted') {
+      var options = {
+        body: 'You have a new notification!',
+        icon: 'images/notification-flat.png',
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+        },
+        actions: [
+          {action: 'open', title: 'Open the site!',
+            icon: 'images/checkmark.png'},
+          {action: 'close', title: 'Go away!',
+            icon: 'images/xmark.png'},
+        ]
+      };
+      registration.showNotification('Hello world!', options);
+      ga('send', 'event', 'notification', 'displayed-standard');
+    } else {
+      console.warn('Notifications are blocked');
+      ga('send', 'event', 'notification', 'blocked');
+    }
+  }
 
-  // TODO Step 8i: Open communication with service worker
+  function openCommunication() {
+    var msgChannel = new MessageChannel();
+    msgChannel.port1.onmessage = function(msgEvent) {
+      sendAnalytics(msgEvent.data);
+    };
+    navigator.serviceWorker.controller.postMessage(
+      'Establishing contact with service worker', [msgChannel.port2]
+    );
+  }
+  openCommunication();
+
+  function sendAnalytics(message) {
+    var eventCategory = message.eventCategory;
+    var eventAction = message.eventAction;
+    ga('send', 'event', eventCategory, eventAction);
+  }
 
   // TODO Step 9b: Add push notification
 
--- service-worker.js
+++ service-worker.js
@@ -3,11 +3,52 @@
 (function() {
   'use strict';
 
-  // TODO Step 8g: Add notification close listener
+  self.addEventListener('notificationclose', function(event) {
+    var notification = event.notification;
+    var primaryKey = notification.data.primaryKey;
+    console.log('Closed notification:', primaryKey);
+    mainClientPort.postMessage({
+      eventCategory: 'notification',
+      eventAction: 'closed'
+    });
+  });
 
-  // TODO Step 8h: Add notification click listener
+  self.addEventListener('notificationclick', function(event) {
+    var notification = event.notification;
+    var primaryKey = notification.data.primaryKey;
+    var action = event.action;
+    if (action === 'close') {
+      notification.close();
+      console.log('Closed notification:', primaryKey);
+      mainClientPort.postMessage({
+        eventCategory: 'notification',
+        eventAction: 'closed'
+      });
+    } else if (action === 'open') {
+      clients.openWindow('pages/page' + primaryKey + '.html');
+      notification.close();
+      console.log('Notification opened');
+      mainClientPort.postMessage({
+        eventCategory: 'notification',
+        eventAction: 'opened'
+      });
+    } else {
+      clients.openWindow('pages/page' + primaryKey + '.html');
+      notification.close();
+      console.log('Notification clicked');
+      mainClientPort.postMessage({
+        eventCategory: 'notification',
+        eventAction: 'clicked'
+      });
+    }
+  });
 
-  // TODO Step 8j: Add message listener & establish communication
+  var mainClientPort;
+  self.addEventListener('message', function(event) {
+    console.log('Service worker received message: ', event.data);
+    mainClientPort = event.ports[0];
+    console.log('Communication ports established');
+  });
 
   // TODO Step 9c: Add push listener
 
